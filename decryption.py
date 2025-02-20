7a87ddc7f0cba11b19e6444d5578496b854505c0
rajesh-1819 <rajesh18raj19@gmail.com>
2025-02-21 00:05:22 +0530

    Update decryption.py

--- decryption.py
+++ decryption.py
@@ -1,42 +1,33 @@
 import cv2
-
-# Load the encrypted image
 img = cv2.imread("encryptedImage.png")
-
-# Ensure image is loaded properly
 if img is None:
     print("Error: Image not found. Make sure 'encryptedImage.png' exists.")
     exit()
 
-# Mapping of numbers to characters
 c = {i: chr(i) for i in range(32, 127)}
-
-# Ask the user for the passcode
 pas = input("Enter passcode for Decryption: ")
-
-# If passcode is correct, begin decryption
 if pas:
     message = ""
     n, m, z = 0, 0, 0
-    height, width, _ = img.shape  # Get image dimensions
+    height, width, _ = img.shape  
 
     while n < height and m < width:
         try:
-            char = c[img[n, m, z]]  # Retrieve character from pixel value
-            if char == "~":  # Stop reading when end marker is found
+            char = c[img[n, m, z]]  
+            if char == "~":  
                 break
             message += char
 
-            z = (z + 1) % 3  # Cycle through RGB channels
+            z = (z + 1) % 3 
             if z == 0:
                 m += 1
                 if m >= width:
                     m = 0
                     n += 1
         except KeyError:
-            break  # Stop if there's an issue
+            break  
         except IndexError:
-            break  # Stop if out of bounds
+            break  
 
     print("Decryption message:", message)
 else:
